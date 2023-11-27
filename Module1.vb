a3f62f337604b44e26ea9f69b220007adebf566d
Nawaf AlKhaldi <H-2w@hotmail.com>
2023-11-27 11:15:16 +0300

    v1.1.0

--- Module1.vb
+++ Module1.vb
@@ -2,22 +2,23 @@
 Imports System.Text.RegularExpressions
 Imports System.Windows.Forms
 Imports WindowsInput
-
 Module Module1
 #Region "AES"
     Const AES_PASSWORD = "YOUR_PASSWORD"
 #End Region
-    Dim written As Boolean = False
     Dim emails, triggerWords, passwords As New List(Of String)
-    Dim txt, logs As New Text.StringBuilder
+    Dim users As New List(Of String)
+    Dim logs As New Text.StringBuilder
     Dim _ex As String
+    Dim sw As New Stopwatch
+    Dim txt As String = String.Empty
     Sub Main()
         Console.ForegroundColor = ConsoleColor.White
-
-        Import()
-
-        Dim th As New Threading.Thread(AddressOf LogKeyStrokes)
+        Dim th As New Threading.Thread(AddressOf Import)
         th.Start()
+
+        Dim th2 As New Threading.Thread(AddressOf LogKeyStrokes)
+        th2.Start()
 
         'Hide Console window
         Dim consoleWindowHandle As IntPtr = FindWindow(Nothing, Console.Title)
@@ -28,32 +29,40 @@ Module Module1
     Private Const SW_HIDE As Integer = 0
     Private Const SW_SHOW As Integer = 5
     Sub Import()
-        Try
-            Dim users As New List(Of String)
-
-            'usersEncrypted is used to save the new list with encrypted passwords
-            Dim usersEncrypted As New List(Of String)
-
-            users = IO.File.ReadAllLines("users.txt").ToList
-            For i = 0 To users.Count - 1
-                Dim email = users(i).Split("|")(0)
-                Dim triggerWord = users(i).Split("|")(1)
-                Dim password = users(i).Split("|")(2)
-                emails.Add(email)
-                triggerWords.Add(triggerWord)
-                passwords.Add(password)
-
-                'Check if the password is already encrypted, If not. Encrypt the password and resave the file
-                If Encryption.AES_Decrypt(password, AES_PASSWORD) = Nothing Then
-                    password = Encryption.AES_Encrypt(password, AES_PASSWORD)
+        While True
+            Try
+                'Check if list is updated or not
+                Dim count = users.Sum(Function(s) s.Length)
+                users = IO.File.ReadAllLines("users.txt").ToList
+                Dim userscount = users.Sum(Function(s) s.Length)
+                If count = userscount Then
+                    Exit Try
                 End If
 
-                usersEncrypted.Add($"{email}|{triggerWord}|{password}")
-                IO.File.WriteAllLines("users.txt", usersEncrypted.ToList)
+                'usersEncrypted is used to save the new list with encrypted passwords
+                Dim usersEncrypted As New List(Of String)
 
-            Next
-        Catch ex As Exception
-        End Try
+                'Get values & encrypt passwords if needed
+                For i = 0 To users.Count - 1
+                    Dim email = users(i).Split("|")(0)
+                    Dim triggerWord = users(i).Split("|")(1)
+                    Dim password = users(i).Split("|")(2)
+                    emails.Add(email)
+                    triggerWords.Add(triggerWord)
+                    passwords.Add(password)
+
+                    'Check if the password is already encrypted, If not. Encrypt the password and resave the file
+                    If Encryption.AES_Decrypt(password, AES_PASSWORD) = Nothing Then
+                        password = Encryption.AES_Encrypt(password, AES_PASSWORD)
+                    End If
+                    usersEncrypted.Add($"{email}|{triggerWord}|{password}")
+                    IO.File.WriteAllLines("users.txt", usersEncrypted.ToList)
+                Next
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            Threading.Thread.Sleep(TimeSpan.FromSeconds(5))
+        End While
     End Sub
     <DllImport("user32.dll")>
     Function GetAsyncKeyState(ByVal vKey As System.Windows.Forms.Keys) As Short
@@ -66,19 +75,18 @@ Module Module1
     Private Function GetWindowText(ByVal hWnd As IntPtr, ByVal lpString As System.Text.StringBuilder, ByVal nMaxCount As Integer) As Integer
     End Function
     Sub LogKeyStrokes()
-
         While True
             Try
-
                 'Get Current Process
                 Dim hWnd As IntPtr = GetForegroundWindow()
                 Dim windowTitle As New System.Text.StringBuilder(256)
                 GetWindowText(hWnd, windowTitle, windowTitle.Capacity)
                 Dim focusedApp As String = windowTitle.ToString()
 
-                If focusedApp.Contains("Battle.net") And written = False Then
+                If focusedApp = "Battle.net Login" Then
                     Console.Title = "Focused Process: Battle.net, Logging Keystrokes..."
 
+#Region "Keylogger"
                     'Keylogger code from: https://github.com/Ritesh18117/Visual-Basic-keylogger
                     Dim key As String
                     Dim i As Integer
@@ -88,53 +96,66 @@ Module Module1
                         result = GetAsyncKeyState(i)
                         If result = -32767 Then
                             result = 0
+                            If i = 8 Then
+                                '[BACKSPACE]
+
+                                If txt.Length > 0 Then
+                                    txt = txt.Substring(0, txt.Length - 1)
+                                    Console.Clear()
+                                    Console.Write(txt)
+                                    Exit For
+                                End If
+                            End If
                             key = Chr(i)
                             If Not Char.IsLetterOrDigit(key) Then
                                 Exit Try
                             End If
                             key = key.ToLower
                             Console.Write(key)
-                            Txt.Append(key)
+                            txt = txt & key
                         End If
                     Next i
+#End Region
 
-                    'Check if typed words contain Keywords
-                    Dim matchingNickname As String = triggerWords.FirstOrDefault(Function(nickname) Regex.IsMatch(Txt.ToString, nickname, RegexOptions.IgnoreCase))
+                    If txt IsNot Nothing Then
 
-                    'Close App keyword
-                    Dim close As String = Regex.IsMatch(Txt.ToString, "closeapp", RegexOptions.IgnoreCase)
-                    If close Then
-                        End
-                    End If
+                        'Check if typed words contain Keywords
+                        Dim matchingNickname As String = triggerWords.FirstOrDefault(Function(nickname) Regex.IsMatch(txt, nickname, RegexOptions.IgnoreCase))
 
-                    If Not String.IsNullOrEmpty(matchingNickname) Then
+                        'Close App keyword
+                        Dim close As String = Regex.IsMatch(txt.ToString, "closeapp", RegexOptions.IgnoreCase)
+                        If close Then
+                            End
+                        End If
 
-                        If triggerWords.Count < 1 Then Exit Sub
-                        written = True
+                        If Not String.IsNullOrEmpty(matchingNickname) Then
+                            If triggerWords.Count < 1 Then Exit Sub
 
-                        txt.Clear()
-                        Dim Email As String = emails(triggerWords.IndexOf(matchingNickname))
-                        Dim Password As String = Encryption.AES_Decrypt(passwords(triggerWords.IndexOf(matchingNickname)), AES_PASSWORD)
+                            txt = Nothing
+                            sw.Start()
+                            Dim Email As String = emails(triggerWords.IndexOf(matchingNickname))
+                            Dim Password As String = Encryption.AES_Decrypt(passwords(triggerWords.IndexOf(matchingNickname)), AES_PASSWORD)
 
-                        Dim inputSimulator As New InputSimulator()
 
-                        'Delete Existing Text
-                        SendKeys.SendWait("^a") : SendKeys.SendWait("{DEL}")
+                            Dim inputSimulator As New InputSimulator()
 
-                        'Type Email
-                        inputSimulator.Keyboard.TextEntry(Email)
+                            'Select all (CTRL + A)
+                            inputSimulator.Keyboard.ModifiedKeyStroke(Native.VirtualKeyCode.CONTROL, Native.VirtualKeyCode.VK_A)
 
-                        'Switch to password input
-                        SendKeys.SendWait("{TAB}")
+                            'Input Email
+                            inputSimulator.Keyboard.TextEntry(Email)
 
-                        'Type Password
-                        inputSimulator.Keyboard.TextEntry(Password)
+                            'Switch to password input
+                            inputSimulator.Keyboard.KeyPress(Native.VirtualKeyCode.TAB)
 
-                        'Login
-                        SendKeys.SendWait("{ENTER}")
-                        Threading.Thread.Sleep(1500)
-                        written = False
+                            Threading.Thread.Sleep(1)
 
+                            'Input Password
+                            inputSimulator.Keyboard.TextEntry(Password)
+
+                            'Login
+                            inputSimulator.Keyboard.KeyPress(Native.VirtualKeyCode.RETURN)
+                        End If
                     End If
                 Else
                     Console.Title = "Focused Process: Else, Stopped..."
@@ -142,7 +163,6 @@ Module Module1
             Catch ex As Exception
                 logError(ex.Message.ToString)
             End Try
-            Threading.Thread.Sleep(25)
         End While
     End Sub
     Sub logError(exception As String)
